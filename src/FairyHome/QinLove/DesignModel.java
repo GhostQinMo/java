f1ba2de319c80bda2ed805b78f4664f20a601004
FairyHome <2095215135@qq.com>
2022-09-05 08:07:13 +0800

    为重学java设计模型创建一个独一分支

---
+++ src/FairyHome/QinLove/DesignModel.java
@@ -0,0 +1,12 @@
+package FairyHome.QinLove;
+
+/**
+ * @author Black_ghost
+ * @title: DesignModel
+ * @projectName DesignModelAll
+ * @description :616  An unchanging God  Qin_Love
+ * @CreateDate 2022-09-05 08:06:44
+ * @Description
+ **/
+public class DesignModel {
+}
