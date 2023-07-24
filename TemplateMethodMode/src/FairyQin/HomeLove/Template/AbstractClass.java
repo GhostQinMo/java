40956389c0efa4061a41af83f36be9034b3911e0
FairyHome <2095215135@qq.com>
2023-07-24 16:48:43 +0800

    AQS中使用了模板方法

--- TemplateMethodMode/src/FairyQin/HomeLove/Template/AbstractClass.java
+++ TemplateMethodMode/src/FairyQin/HomeLove/Template/AbstractClass.java
@@ -9,6 +9,7 @@ package FairyQin.HomeLove.Template;
  * @Description 模板方法中抽象类角色
  **/
 public abstract class AbstractClass {
+    //复习AQS的时候看到AQS中使用了模板方法设计模式
     //模板方法 可以定义为final修饰的，防止子类修改
     public  final void cookProcess(){
         pourOil();
@@ -25,6 +26,7 @@ public abstract class AbstractClass {
     public  final void headOil(){
         System.out.println("热油………………");
     }
+
     //放入菜品，定义规范，有子类实现
     public  abstract void pourVagetable();
 
