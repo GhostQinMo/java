0999a53bc89f0c332ac5e4df3d6f64c7945b3e2d
AngelXin <2095215135@qq.com>
2024-03-30 18:23:52 +0800

    JUC:Use Atomic and Immutable class ,How to design Immutable class

--- JUCModeAll/TwoPhaseTermination/src/main/java/FairyQin/HomeLove/TwoPhaseTerminationTest.java
+++ JUCModeAll/TwoPhaseTermination/src/main/java/FairyQin/HomeLove/TwoPhaseTerminationTest.java
@@ -1,6 +1,9 @@
 package FairyQin.HomeLove;
 
+import java.math.BigDecimal;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicMarkableReference;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author Black_ghost
@@ -16,6 +19,6 @@ public class TwoPhaseTerminationTest {
         twoPhaseTermination.start();
         TimeUnit.SECONDS.sleep(4);
         //打断线程   这里的stop方法是自己定义的
-        twoPhaseTermination.stop();
+        BigDecimal bigDecimal=new BigDecimal(1000_000_000);
     }
 }
